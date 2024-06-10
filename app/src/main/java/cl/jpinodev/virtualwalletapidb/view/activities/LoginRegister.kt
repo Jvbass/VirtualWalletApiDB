92c632365ca935dae2697e511bb277a494710d13
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-06-09 22:13:23 -0400

    Se crean activities splash, login, login register

---
+++ app/src/main/java/cl/jpinodev/virtualwalletapidb/view/activities/LoginRegister.kt
@@ -0,0 +1,21 @@
+package cl.jpinodev.virtualwalletapidb.view.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import cl.jpinodev.virtualwalletapidb.R
+
+class LoginRegister : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
